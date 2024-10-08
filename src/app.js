e0351ea9f85c600bf3f9534bf60d5a7f51bd050e
Alejandro <78373978+C20ALEX00@users.noreply.github.com>
2024-10-08 14:20:35 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronom = ["El", "Los", "Las", "Tu"];
+  let nom = ["buen", "mal", "insano", "confiable"];
+  let adj = ["financiero", "fritos", "samurai", "financiero", "sofa"];
+  let ext = [".com", ".es", ".net"];
+
+  for (let i = 0; i < pronom.length; i++) {
+    for (let j = 0; j < nom.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          console.log(pronom[i] + nom[j] + adj[k] + ext[l]);
+        }
+      }
+    }
+  }
+
+  document.querySelector("#fraseDominios").innerHTML =
+    pronom[i] + nom[j] + adj[k] + ext[l];
+
+  document.getElementById("generador").addEventListener("click", window.onload);
 };
