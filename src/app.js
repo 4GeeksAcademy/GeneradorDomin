c03e1b5488f2934a7ab7bfadb63cf4e95114b991
Alejandro <78373978+C20ALEX00@users.noreply.github.com>
2024-10-12 13:19:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -10,20 +10,22 @@ window.onload = function() {
   let pronom = ["El", "Los", "Las", "Tu"];
   let nom = ["buen", "mal", "insano", "confiable"];
   let adj = ["financiero", "fritos", "samurai", "financiero", "sofa"];
-  let ext = [".com", ".es", ".net"];
+  let dom = [".com", ".es", ".net"];
 
-  for (let i = 0; i < pronom.length; i++) {
-    for (let j = 0; j < nom.length; j++) {
-      for (let k = 0; k < adj.length; k++) {
-        for (let l = 0; l < ext.length; l++) {
-          console.log(pronom[i] + nom[j] + adj[k] + ext[l]);
+  function generatorDom() {
+    for (let i = 0; i < pronom.length; i++) {
+      for (let j = 0; j < nom.length; j++) {
+        for (let k = 0; k < adj.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            console.log(pronom[i] + nom[j] + adj[k] + dom[l]);
+            document.querySelector("generatorDom").innerHTML =
+              pronom[i] + nom[j] + adj[k] + dom[l];
+            document
+              .getElementById("generador")
+              .addEventListener("click", window.onload);
+          }
         }
       }
     }
   }
-
-  document.querySelector("#fraseDominios").innerHTML =
-    pronom[i] + nom[j] + adj[k] + ext[l];
-
-  document.getElementById("generador").addEventListener("click", window.onload);
 };
