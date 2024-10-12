aa1b74bc03ac74b1afe7c08bb427ca0ed33a3217
Alejandro <78373978+C20ALEX00@users.noreply.github.com>
2024-10-12 15:26:41 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,26 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  // variables para los dominios
   let pronom = ["El", "Los", "Las", "Tu"];
   let nom = ["buen", "mal", "insano", "confiable"];
   let adj = ["financiero", "fritos", "samurai", "financiero", "sofa"];
   let dom = [".com", ".es", ".net"];
 
-  function generatorDom() {
-    for (let i = 0; i < pronom.length; i++) {
-      for (let j = 0; j < nom.length; j++) {
-        for (let k = 0; k < adj.length; k++) {
-          for (let l = 0; l < dom.length; l++) {
-            console.log(pronom[i] + nom[j] + adj[k] + dom[l]);
-            document.querySelector("generatorDom").innerHTML =
-              pronom[i] + nom[j] + adj[k] + dom[l];
-            document
-              .getElementById("generador")
-              .addEventListener("click", window.onload);
-          }
+  //un let para la variable fraseDominios con el id del h5 y otro let para definir la variable fraseGenerada
+  let fraseDominios = document.getElementById("fraseDominios");
+  let fraseGenerada = "";
+
+  // bucle
+  for (let i = 0; i < pronom.length; i++) {
+    for (let j = 0; j < nom.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          fraseGenerada += pronom[i] + nom[j] + adj[k] + dom[l] + " ";
         }
       }
     }
   }
+  fraseDominios.innerHTML = fraseGenerada;
 };
